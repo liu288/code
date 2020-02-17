05b2d1fe69fdb5c8f2a1613b922d14ae051e6e79
liu288 <935622585@qq.com>
2020-02-17 21:52:10 +0800

    增强swaggerUI配置

--- springboot/src/main/java/com/springboot/demo/Swagger2Config.java
+++ springboot/src/main/java/com/springboot/demo/Swagger2Config.java
@@ -2,44 +2,75 @@ package com.springboot.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
 @EnableSwagger2
-public class Swagger2Config {
+public class Swagger2Config extends WebMvcConfigurerAdapter {
+
+//    @Bean
+//    public Docket createRestApi() {
+//        List<Parameter> pars = new ArrayList<Parameter>();
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .globalOperationParameters(pars)
+//                .apiInfo(apiInfo());
+//    }
 
     @Bean
-    public Docket createRestApi() {
-        List<Parameter> pars = new ArrayList<Parameter>();
+    public Docket createH5RestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .groupName("h5")
                 .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.demo.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .globalOperationParameters(pars)
-                .apiInfo(apiInfo());
+                .build();
+    }
+
+    @Bean
+    public Docket createPcRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .groupName("pc")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.demo.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("RESTful API")
-                .description("展示先做基础功能，后面再添加业务")
-                .termsOfServiceUrl("https://www.cnblogs.com/xiebq/")
+                .title("Demo测试")
+                .description("学习，测试")
+                .contact("LiuXiao")
+                .termsOfServiceUrl("https://localhost/")
                 .version("1.0")
                 .build();
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
 
 }
